204909b76918f37af92d6ac592bcbddd093608e6
tstkokki <60537602+tstkokki@users.noreply.github.com>
2020-04-27 09:49:15 +0300

    stuff

---
+++ glStuff/openGLclassTutorial/ogl-master/masterClass/launch-playground-Debug.cmd
@@ -0,0 +1,9 @@
+cd D:/glStuff/openGLclassTutorial/ogl-master/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"D:\glStuff\openGLclassTutorial\ogl-master\masterClass\Debug\playground.exe"  
+pause
---
+++ glStuff/openGLclassTutorial/ogl-master/masterClass/launch-playground-MinSizeRel.cmd
@@ -0,0 +1,9 @@
+cd D:/glStuff/openGLclassTutorial/ogl-master/playground/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"D:\glStuff\openGLclassTutorial\ogl-master\masterClass\MinSizeRel\playground.exe"  
+pause
